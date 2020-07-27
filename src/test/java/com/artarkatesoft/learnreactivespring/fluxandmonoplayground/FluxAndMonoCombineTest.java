7298a6b99c3be47c6cf17a21026647fa570fed19
admin <d.art.shishkin@gmail.com>
2020-07-27 12:49:15 +0300

    Combining Reactive Streams (20)

---
+++ src/test/java/com/artarkatesoft/learnreactivespring/fluxandmonoplayground/FluxAndMonoCombineTest.java
@@ -0,0 +1,68 @@
+package com.artarkatesoft.learnreactivespring.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class FluxAndMonoCombineTest {
+
+    @Test
+    void combineUsingMerge() {
+        Flux<String> flux1 = Flux.just("A", "B", "C");
+        Flux<String> flux2 = Flux.just("1", "2", "3");
+        Flux<String> mergeFlux = Flux.merge(flux1, flux2);
+        StepVerifier.create(mergeFlux.log())
+                .expectSubscription()
+                .expectNextCount(6)
+                .verifyComplete();
+    }
+
+    @Test
+    void combineUsingMerge_withDelay() {
+        Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
+        Flux<String> flux2 = Flux.just("1", "2", "3").delayElements(Duration.ofSeconds(1));
+        Flux<String> mergeFlux = Flux.merge(flux1, flux2);
+        StepVerifier.create(mergeFlux.log())
+                .expectSubscription()
+                .expectNextCount(6)
+                .verifyComplete();
+    }
+
+    @Test
+    void combineUsingConcat() {
+        Flux<String> flux1 = Flux.just("A", "B", "C");
+        Flux<String> flux2 = Flux.just("1", "2", "3");
+        Flux<String> concatFlux = Flux.concat(flux1, flux2);
+        StepVerifier.create(concatFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C", "1", "2", "3")
+                .verifyComplete();
+    }
+
+    @Test
+    void combineUsingConcat_withDelay() {
+        Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1)).log();
+        Flux<String> flux2 = Flux.just("1", "2", "3").delayElements(Duration.ofSeconds(1)).log();
+        Flux<String> concatFlux = Flux.concat(flux1, flux2);
+        StepVerifier.create(concatFlux)
+                .expectSubscription()
+                .expectNext("A", "B", "C", "1", "2", "3")
+                .verifyComplete();
+    }
+
+    @Test
+    void combineUsingZip() {
+        Flux<String> flux1 = Flux.just("A", "B", "C");
+        Flux<String> flux2 = Flux.just("1", "2", "3");
+
+        Flux<String> zipFlux = Flux.zip(flux1, flux2, (t1, t2) -> t1.concat(t2));
+
+        StepVerifier.create(zipFlux.log())
+                .expectSubscription()
+                .expectNext("A1", "B2", "C3")
+                .verifyComplete();
+    }
+
+}
