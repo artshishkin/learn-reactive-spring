088b981ec5d6ed18569b8ae7b34149f2f26d2239
admin <d.art.shishkin@gmail.com>
2020-07-28 17:33:57 +0300

    Initialize data using CommandLineRunner (52)

---
+++ src/main/java/com/artarkatesoft/learnreactivespring/bootstrap/ItemDataInitializer.java
@@ -0,0 +1,36 @@
+package com.artarkatesoft.learnreactivespring.bootstrap;
+
+import com.artarkatesoft.learnreactivespring.documents.Item;
+import com.artarkatesoft.learnreactivespring.repositories.ItemReactiveRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+@Component
+@RequiredArgsConstructor
+public class ItemDataInitializer implements CommandLineRunner {
+
+    private final ItemReactiveRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        bootstrapItemData();
+    }
+
+    private void bootstrapItemData() {
+        Stream<Item> itemStream = IntStream.rangeClosed(1, 5)
+                .mapToObj(i -> new Item(null, "desc" + i, 111.11 * i));
+        Item defaultItem = new Item(UUID.randomUUID().toString(), "Default Description", 999.99);
+
+        repository.deleteAll()
+                .thenMany(Flux.fromStream(itemStream).concatWith(Flux.just(defaultItem)))
+                .flatMap(repository::save)
+                .log("bootstrapItemData")
+                .subscribe();
+    }
+}
