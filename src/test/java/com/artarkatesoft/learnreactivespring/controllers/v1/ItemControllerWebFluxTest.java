aae0a4b7174cbba85e944f4383c50382b032104d
admin <d.art.shishkin@gmail.com>
2020-07-28 20:30:40 +0300

    Added WebFluxTest on ItemController #7

---
+++ src/test/java/com/artarkatesoft/learnreactivespring/controllers/v1/ItemControllerWebFluxTest.java
@@ -0,0 +1,83 @@
+package com.artarkatesoft.learnreactivespring.controllers.v1;
+
+import com.artarkatesoft.learnreactivespring.documents.Item;
+import com.artarkatesoft.learnreactivespring.repositories.ItemReactiveRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.artarkatesoft.learnreactivespring.constants.ItemConstants.ITEM_END_POINT_V1;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@WebFluxTest(controllers = ItemController.class)
+class ItemControllerWebFluxTest {
+
+    @MockBean
+    ItemReactiveRepository itemRepository;
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    private Item defaultItem;
+    private Flux<Item> repositoryFlux;
+    private List<Item> itemsInRepo;
+
+    @BeforeEach
+    void setUp() {
+        defaultItem = new Item("MyId", "desc4", 123.99);
+        itemsInRepo = IntStream
+                .rangeClosed(1, 5)
+                .mapToObj(i -> new Item("id" + i, "desc" + i, (double) (i * 111)))
+                .collect(Collectors.toList());
+        itemsInRepo.add(defaultItem);
+        repositoryFlux = Flux.fromIterable(itemsInRepo);
+    }
+
+    @Test
+    void getAllItemsTest() {
+        //given
+        given(itemRepository.findAll()).willReturn(repositoryFlux);
+        //when
+        EntityExchangeResult<List<Item>> result = webTestClient.get().uri(ITEM_END_POINT_V1)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Item.class)
+                .hasSize(6)
+                .returnResult();
+        //then
+        then(itemRepository).should().findAll();
+        assertThat(result.getResponseBody()).containsExactlyInAnyOrderElementsOf(itemsInRepo);
+    }
+
+    @Test
+    void getAllItemsTest_approach2() {
+        //given
+        given(itemRepository.findAll()).willReturn(repositoryFlux);
+        //when
+        Flux<Item> itemFlux = webTestClient.get().uri(ITEM_END_POINT_V1)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Item.class)
+                .getResponseBody();
+        //then
+        StepVerifier.create(itemFlux)
+                .expectNextCount(6)
+                .verifyComplete();
+        then(itemRepository).should().findAll();
+    }
+}
