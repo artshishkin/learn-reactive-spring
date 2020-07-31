589b770d96aa6529f465e660bea1ee4673fc946d
Art <d.art.shishkin@gmail.com>
2020-07-31 12:44:05 +0300

    added test case for accept TEXT_PLAIN scenario of runtime exception endpoint. closes #13

--- src/test/java/com/artarkatesoft/learnreactivespring/controllers/v1/ItemSampleExceptionControllerWebFluxTest.java
+++ src/test/java/com/artarkatesoft/learnreactivespring/controllers/v1/ItemSampleExceptionControllerWebFluxTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static com.artarkatesoft.learnreactivespring.controllers.v1.ItemSampleExceptionController.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.TEXT_PLAIN;
 
 @WebFluxTest(controllers = ItemSampleExceptionController.class)
 class ItemSampleExceptionControllerWebFluxTest {
@@ -46,9 +47,18 @@ class ItemSampleExceptionControllerWebFluxTest {
     }
 
     @Test
-    void runtimeException() {
+    void runtimeException_json() {
         webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1+"/runtimeException")
                 .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().is5xxServerError()
+                .expectBody(String.class)
+                .isEqualTo(RUNTIME_EXCEPTION_MESSAGE);
+    }
+    @Test
+    void runtimeException_text() {
+        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1+"/runtimeException")
+                .accept(TEXT_PLAIN)
                 .exchange()
                 .expectStatus().is5xxServerError()
                 .expectBody(String.class)
